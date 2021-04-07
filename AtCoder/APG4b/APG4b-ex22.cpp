b9e8de8d7dd980806f57b7010ad74055a08597b8
yo1956 <hm.hr.yossy@gmail.com>
2021-04-07 22:18:33 +0900

    ex22

---
+++ AtCoder/APG4b/APG4b-ex22.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int a, b;
+	vector<pair<int, int>> ab(N);
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		//ab.push_back(make_pair(b, a));
+		ab[i].first = b;
+		ab[i].second = a;
+	}
+
+	sort(ab.begin(), ab.end());
+
+	for (int i = 0; i < N; i++) {
+		swap(ab[i].first, ab[i].second);
+		cout << ab[i].first << " " << ab[i].second << endl;
+	}
+}
