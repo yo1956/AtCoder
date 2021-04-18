cd15b1816bf3e3adc6b251de48de25fa9fa5c9fc
yo1956 <hm.hr.yossy@gmail.com>
2021-04-18 22:58:24 +0900

    ARC117

---
+++ AtCoder/ARC117/ARC117-B.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> A(N,0);
+	int max = -1; //最高階数
+
+	int cnt = 1; //景観の数 //初期状態を数えて1からとする
+
+	for (int i = 0; i < N; ++i) {
+		cin >> A[i];
+
+		if (A[i] > max)
+			max = A[i];
+	}
+		
+	//vector<int> calc = A;
+	////高さが全部0になったら終わり
+	//while (max >= 1) {
+	//	for (int i = max; i > 0; --i) {
+
+	//	}
+
+	//	--max;
+	//}
+	
+
+}
